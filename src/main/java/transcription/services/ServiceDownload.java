8bbf52344281b7ffcf61a94e1475dc171c65d63a
Hywel Griffiths <hywel.griffiths@dvsa.gov.uk>
2020-07-31 06:50:00 +0100

    Small changes and version updated

--- src/main/java/transcription/services/ServiceDownload.java
+++ src/main/java/transcription/services/ServiceDownload.java
@@ -11,10 +11,12 @@ import java.util.logging.Logger;
 
 public class ServiceDownload {
     ProviderDownload providerDownload = new ProviderDownload();
-
+    private String notTrancribed = "File is not transcribed yet.";
+    private String notExist = "Job name does not exist";
     /**
      * This method sends the request for a download to the provider. It then writes a file using the transcription text
      * that is returned by the server.
+     *
      * @param job
      * @param filePath
      */
@@ -23,13 +25,13 @@ public class ServiceDownload {
     public String sendDownloadHttp(String job, String filePath) {
         try {
             String json = providerDownload.executeDownloadHttp(job + ".json");
-            if (json.length()>34 && json.substring(1,34).equals("java.nio.file.NoSuchFileException")){
-                return "File is not transcribed yet.";
+            if (json.length() > 34 && json.substring(1, 34).equals("java.nio.file.NoSuchFileException")) {
+                return notTrancribed;
             }
-            if (json.equals("\"File does not exist\"")){
-                return "Job name does not exist";
+            if (json.equals("\"File does not exist\"")) {
+                return notExist;
             }
-            Files.write( Paths.get(filePath+"/"+job+".txt"), providerDownload.executeDownloadHttp(job + ".json").getBytes());
+            Files.write(Paths.get(filePath + "/" + job + ".txt"), providerDownload.executeDownloadHttp(job + ".json").getBytes());
             LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.INFO);
             LOGGER.info("complete");
             return json;
--- src/test/java/transcription/services/ServiceUploadTest.java
+++ src/test/java/transcription/services/ServiceUploadTest.java
@@ -2,6 +2,7 @@ package transcription.services;
 
 import mockit.Mock;
 import mockit.MockUp;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import transcription.providers.ProviderUpload;
@@ -17,6 +18,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ServiceUploadTest {
     private String filePath = "src/test/resources/AudioTestFile.mp3";
     private ServiceUpload serviceUpload = new ServiceUpload();
+    private static final String JOBNAME = "JOBNAME";
+    private static final String TESTFILE = "src/test/java/resources/fakeMp4.txt";
 
     @Test
     @DisplayName("TestConversionOfMp4ToMp3")
@@ -25,6 +28,7 @@ public class ServiceUploadTest {
         new MockUp<Encoder>() {
             @Mock
             public void encode(MultimediaObject multimediaObject, File target, EncodingAttributes attributes) throws IllegalArgumentException, InputFormatException, EncoderException {
+                //Do nothing
             }
         };
         assertEquals("src/test/resources/Audio.mp3", serviceUpload.convertToMp3(filePath));
@@ -33,25 +37,24 @@ public class ServiceUploadTest {
     @Test
     @DisplayName("Test cutting loop when length is over 5000000")
     void testLongCuttingLoop() throws IOException {
-        InputStream inputStream = new FileInputStream("/Users/hywelgriffiths/Documents/IntellijProjects/sipho/transcriptionSoftware/audio.transcribe.front/src/test/java/resources/base64.txt");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String base64 = bufferedReader.readLine();
-        ServiceUpload serviceUpload = new ServiceUpload();
+        InputStream inputStream = new FileInputStream("/Users/hywelgriffiths/Documents/IntellijProjects/sipho/transcriptionSoftware/AudioTranscribeFront/src/test/java/resources/base64.txt");
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-        new MockUp<ProviderUpload>() {
-            @Mock
-            public String executeUploadHttp(String mp3Base64, String jobName, String tag, String email) {
-                return null;
-            }
-        };
+            String base64 = bufferedReader.readLine();
 
-        assertNull(serviceUpload.cuttingLoop(base64, "JOBNAME", null));
+            new MockUp<ProviderUpload>() {
+                @Mock
+                public String executeUploadHttp(String mp3Base64, String jobName, String tag, String email) {
+                    return null;
+                }
+            };
+            assertNull(serviceUpload.cuttingLoop(base64, JOBNAME, null));
+        }
     }
 
     @Test
     @DisplayName("Test cutting loop when length is under 5000000")
-    void testShortCuttingLoop() throws IOException {
-        ServiceUpload serviceUpload = new ServiceUpload();
+    void testShortCuttingLoop(){
 
         new MockUp<ProviderUpload>() {
             @Mock
@@ -60,39 +63,36 @@ public class ServiceUploadTest {
             }
         };
 
-        assertNull(serviceUpload.cuttingLoop("SHORTBASE64", "JOBNAME", null));
+        assertNull(serviceUpload.cuttingLoop("SHORTBASE64", JOBNAME, null));
     }
 
     @Test
     @DisplayName("Test convertToBase64AndSend")
     void testConvertToBase64AndSend() throws IOException {
-        ServiceUpload serviceUpload = new ServiceUpload();
-        File file = new File("src/test/java/resources/fakeMp4.txt");
-        String jobName = "JOBNAME";
+        File file = new File(TESTFILE);
+        String jobName = JOBNAME;
 
         new MockUp<ServiceUpload>() {
             @Mock
             public String convertToMp3(String mp4File) {
-                return "src/test/java/resources/fakeMp4.txt";
+                return TESTFILE;
             }
         };
 
         assertEquals("\"complete\"", serviceUpload.convertToBase64AndSend(jobName, file, null, false));
-        File newFile  = new File("src/test/java/resources/fakeMp4.txt");
-        newFile.createNewFile();
+        new File(TESTFILE).createNewFile();
     }
 
     @Test
     @DisplayName("Test convertToBase64andSendCatchBlock")
     void testConvertToBase64AndSendCatch() {
-        ServiceUpload serviceUpload = new ServiceUpload();
-        File file = new File("src/test/java/resources/fakeMp4.txt");
-        String jobName = "JOBNAME";
+        File file = new File(TESTFILE);
+        String jobName = JOBNAME;
 
         new MockUp<ServiceUpload>() {
             @Mock
-            public String convertToMp3(String mp4File) throws Exception {
-                throw new Exception("Forced Exception");
+            public String convertToMp3(String mp4File) throws IOException {
+                throw new IOException("Forced Exception");
             }
         };
 
@@ -105,8 +105,8 @@ public class ServiceUploadTest {
 
         new MockUp<ServiceUpload>() {
             @Mock
-            public String createMp3(String mp4file) throws Exception {
-                throw new Exception("Forced Exception");
+            public String createMp3(String mp4file) throws IOException {
+                throw new IOException("Forced Exception");
             }
         };
 
@@ -114,7 +114,7 @@ public class ServiceUploadTest {
     }
 
     @Test
-    @DisplayName("any")
+    @DisplayName("Test youtube")
     void testYoutube() {
 
         new MockUp<URL>(){
